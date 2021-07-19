591bcbc5ef8b15c54369e1449ea46a170af49200
Kacper <kacpersob18@gmail.com>
2021-07-19 22:10:49 +0200

    New version

--- app/src/androidTest/java/com/example/myapplication/CalculationsTest.kt
+++ app/src/androidTest/java/com/example/myapplication/CalculationsTest.kt
@@ -1,5 +1,6 @@
-package com.example.myapplication.calculations
+package com.example.myapplication
 
+import com.example.myapplication.calculations.Calculations
 import junit.framework.TestCase
 import org.junit.Test
 import kotlin.math.roundToInt
@@ -11,10 +12,10 @@ class CalculationsTest : TestCase() {
     fun testCalculations() {
         //values taken from site: https://www.omnicalculator.com/other/azimuth
         val expectedAzimuth = 94
-        val startLati = 36.45773
-        val startLong = -113.32985
-        val finalLati = 37.3565
-        val finalLong = -121.9618
+        val startLati = 37.3565
+        val startLong = -121.9618
+        val finalLati = 36.45773
+        val finalLong = -113.32985
         val calculatedAzimuth = calculations.calculateAzimuth(startLati, startLong, finalLati, finalLong)
         assertEquals(expectedAzimuth, calculatedAzimuth)
         val expectedDistance = 774
